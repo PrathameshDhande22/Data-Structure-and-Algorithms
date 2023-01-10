8904e3066f2ac16315f997801074aea7e2a0a65c
Prathamesh Dhande <prathameshdhande534@gmail.com>
2023-01-10 22:37:14 +0530

    Factors and Squareroot problem,code

---
+++ src/tutorial/Factors.java
@@ -0,0 +1,45 @@
+package tutorial;
+
+public class Factors {
+
+	public static void main(String[] args) {
+		int no = 36;
+		Fact1(no);
+		Fact2(no);
+	}
+
+	/**
+	 * The function generates all the factors of the number. <br>
+	 * But This function takes O(n) time so {@code Fact2(int no)} is the optimised
+	 * code.
+	 * 
+	 * @param no : Number
+	 * 
+	 */
+	public static void Fact1(int no) {
+		for (int i = 1; i <= no; i++) {
+			if (no % i == 0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	/**
+	 * This function is the optimsed code for Factors of Number and it takes
+	 * O(sqrt(no)) time.
+	 * 
+	 * @param no : Number
+	 */
+	public static void Fact2(int no) {
+		for (int i = 1; i <= Math.sqrt(no); i++) {
+			if (no % i == 0) {
+				if (no / i == i) {
+					System.out.print(i + ",");
+				} else {
+					System.out.print(i + "," + no / i + ",");
+				}
+			}
+		}
+	}
+
+}
---
+++ src/tutorial/SquareRoot.java
@@ -0,0 +1,31 @@
+package tutorial;
+
+public class SquareRoot {
+
+	public static void main(String[] args) {
+		int no = 36;
+		System.out.println(Math.sqrt(no));
+		System.out.println(sqrt(no));
+	}
+
+	/**
+	 * this function carry out the sqrt function just like
+	 * <code>Math.sqrt(40);</code>
+	 * 
+	 * @param no : Number of which you want to find the square root
+	 * @return {@link Double} square root of the no.
+	 */
+	public static double sqrt(int no) {
+		double x = no;
+		double root;
+		while (true) {
+			root = (x + no / x) / 2;
+			if (Math.abs(root - x) < 0.0005) {
+				break;
+			}
+			x = root;
+		}
+		return root;
+	}
+
+}
