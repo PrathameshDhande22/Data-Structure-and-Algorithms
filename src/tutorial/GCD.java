7550ade92d3801215d97469a0b75625579f83654
Prathamesh Dhande <prathameshdhande534@gmail.com>
2023-02-14 23:45:35 +0530

    Extended Euclidean Algorithm

--- src/tutorial/GCD.java
+++ src/tutorial/GCD.java
@@ -1,5 +1,7 @@
 package tutorial;
 
+import java.util.Arrays;
+
 public class GCD {
 
 	/**
@@ -46,10 +48,50 @@ public class GCD {
 		return a * b / gcd(a, b);
 	}
 
+	/**
+	 * Implemented the Extended Euclidean Algorithm to find the gcd of two numbers.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return Array of [gcd,s,t] value
+	 */
+	public static int[] extendedEuclid(int a, int b) {
+		int rem = 0, s2 = 0, s1 = 1, s = 0, t1 = 0, t2 = 1, t = 0, quo = 0;
+		while (b != 0) {
+			quo = (int) Math.abs((double) (a / b));
+			rem = a % b;
+			s = s1 - (quo * s2);
+			t = t1 - t2 * quo;
+			System.out.printf("rem=%d,s=%d,t=%d,s1=%d,s2=%d,t1=%d,t2=%d,quo=%d,a=%d,b=%d\n", rem, s, t, s1, s2, t1, t2,
+					quo, a, b);
+			a = b;
+			b = rem;
+			s1 = s2;
+			s2 = s;
+			t1 = t2;
+			t2 = t;
+		}
+		return new int[] { a, s1, t1 };
+	}
+
+	/**
+	 * This Function is used to verify the extend euclidean algorithm
+	 * 
+	 * @param ans
+	 * @param a
+	 * @param b
+	 * @return True if is right or false.
+	 */
+	public static boolean verify(int[] ans, int a, int b) {
+		return ans[1] * a + ans[2] * b == gcd(a, b);
+	}
+
 	public static void main(String[] args) {
 		System.out.println(gcd(3, 9));
 		System.out.println(recursivegcd(3, 5));
 		System.out.println(lcm(2, 4));
+		System.out.println(Arrays.toString(extendedEuclid(161, 28)));
+		System.out.println(verify(extendedEuclid(161, 28), 161, 28));
 	}
 
 }
